3f3600ceec8866c6da115d4832faa5245e5dec20
jgp7481 <102352541+jgp7481@users.noreply.github.com>
2023-04-29 23:09:57 +0530

    Add files via upload

---
+++ D1.cpp
@@ -0,0 +1,25 @@
+#include<iostram>
+using namespace std;
+
+int main()
+{
+    int n, a = 0, d = 0, flag = 0;;
+    string r;
+
+    cin>>n;
+    gets(r);
+
+    for(int i = 0; r[i]!='\n'; i++)
+    {
+        if(r[i] == 'A') a++;
+        else if (r[i] == 'D') d++;
+        else flag = 1;
+    }
+    if(flag) cout << "Invalid input..";
+    else
+    {
+        if(a>d)cout<<"Anton";
+        else if(d>a)cout<<"Danik";
+        else cout<<"Friendship";
+    }
+}
